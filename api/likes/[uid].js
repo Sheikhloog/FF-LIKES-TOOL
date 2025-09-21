46aa72d32ffd101765af4c965a8866a7c2eda8b7
Sheikhloog <121605563+Sheikhloog@users.noreply.github.com>
2025-09-21 16:11:59 +0500

    Update [uid].js

--- api/likes/[uid].js
+++ api/likes/[uid].js
@@ -1,19 +1,20 @@
-export default async function handler(req, res) {
-  const fs = require('fs');
-  const path = require('path');
-  const filePath = path.join(process.cwd(), 'likes.json');
+const fs = require("fs");
+const path = require("path");
+
+module.exports = async function (req, res) {
+  const filePath = path.join(process.cwd(), "likes.json");
 
   if (!fs.existsSync(filePath)) {
     return res.status(200).json({});
   }
 
-  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
   const { uid } = req.query;
   const record = data[uid];
 
   if (!record) {
-    return res.status(404).json({ error: 'UID not found' });
+    return res.status(404).json({ error: "UID not found" });
   }
 
   return res.status(200).json(record);
-}
+};
